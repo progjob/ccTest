a60a0094ba165fad36057f52aa2bc87ddc1a5b09
progjob <jonedoe@macMax.local>
2015-04-30 12:05:45 +0300

    new struct and print procedure

--- structRecursiveTest/main.c
+++ structRecursiveTest/main.c
@@ -8,8 +8,32 @@
 
 #include <stdio.h>
 
+typedef struct charItem charItem;
+
+struct charItem {
+    char ch;
+    charItem *nextCahr;
+};
+
+void outCharItem(const charItem *c, int tabsCount) {
+    for (int i = 0; i < tabsCount; i++)
+        printf("\t");
+    printf("%c\n", c->ch);
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    printf("__\n\n");
+    
+    charItem s1, s2, s3;
+    s1.ch = 's';
+    s2.ch = 'j';
+    s3.ch = 'o';
+    
+    charItem charItems[] = {s1, s2, s3};
+    for (int i = 0; i < 3; i++)
+        outCharItem(&charItems[i], i);
+    
+    
+    printf("\n__\n");
     return 0;
 }
