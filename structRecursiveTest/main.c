7b23c60d1ce42dff9e526a9c3ed1b20e8adb94ec
progjob <jonedoe@macMax.local>
2015-04-30 11:49:53 +0300

    Initial Commit

---
+++ structRecursiveTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  structRecursiveTest
+//
+//  Created by Jone Doe on 30/04/15.
+//  Copyright (c) 2015 Jone Doe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
